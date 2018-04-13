a0e30636347ab923f1442393ede80785d3602453
Tom Dugan <thomas@aadwebb.com>
2018-04-12 19:31:53 -0500

    populated pets.js

---
+++ javascripts/pets.js
@@ -0,0 +1,9 @@
+const loadDinos = (loadFunction, errorFunction) => {
+  const myRequest = new XMLHttpRequest();
+  myRequest.addEventListener('load', loadFunction);
+  myRequest.addEventListener('error', errorFunction);
+  myRequest.open('GET', '../db/pets.json');
+  myRequest.send();
+};
+
+module.exports = loadDinos;
